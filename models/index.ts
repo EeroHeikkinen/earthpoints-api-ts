5defbba84f15e3400de39bcaf806d0047d6a01f8
Eero Heikkinen <eero@kamupak.com>
2022-03-19 23:50:26 +0200

    Initial commit

---
+++ models/index.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ClientCredentialsDto';
+export * from './ClientCredentialsResponseDto';
+export * from './CreatePlatformConnectionDto';
+export * from './CreatePointEventDto';
+export * from './CreatePointEventResponseDto';
+export * from './CreateUserDto';
+export * from './PointEvent';
+export * from './UpdatePointEventDto';
+export * from './User';
+export * from './UserFromExternalPlatformDataDto';
